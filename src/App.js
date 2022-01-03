7f4f63f7e60b02425fb14038f2a10925a4728984
dania-c <workingdania@hotmail.com>
2022-01-03 13:36:51 +0200

    load the cards pic in a grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  // created outside the component cause it will no tgonna change+ it will not be affected if the app component is rerendered
+  { "src": "/img/helmet-1.png" },
+  { "src": "/img/potion-1.png" },
+  { "src": "/img/ring-1.png" },
+  { "src": "/img/scroll-1.png" },
+  { "src": "/img/shield-1.png" },
+  { "src": "/img/sword-1.png" }
+]
+
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)  //shuffle cards
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+  // console.log(cards, turns)
   return (
     <div className="App">
-      1
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className='card' key={card.id}>
+            <img className='front' src={card.src} alt="card front" />
+            <img className='back' src="/img/cover.png" alt="card front" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
