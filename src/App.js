fa15ee9f30388065528f3294c98d743ad61dfd5a
dania-c <workingdania@hotmail.com>
2022-01-03 15:43:31 +0200

    console.log match/nomatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
   // created outside the component cause it will no tgonna change+ it will not be affected if the app component is rerendered
@@ -15,7 +16,11 @@ const cardImages = [
 
 function App() {
   const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)  //shuffle cards
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+  //shuffle cards
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -25,17 +30,47 @@ function App() {
     setCards(shuffledCards)
     setTurns(0)
   }
-  // console.log(cards, turns)
+
+  //handle choices
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("match", choiceOne.src)
+
+      } else {
+        console.log("nomatch")
+
+      }
+      resetTurn()
+    }
+  }, [choiceOne, choiceTwo])
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1)
+    console.log(turns)
+  }
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <div className='card' key={card.id}>
-            <img className='front' src={card.src} alt="card front" />
-            <img className='back' src="/img/cover.png" alt="card front" />
-          </div>
+          // <div className='card' key={card.id}>
+          //   <img className='front' src={card.src} alt="card front" />
+          //   <img className='back' src="/img/cover.png" alt="card back" />
+          // </div>
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+          />
         ))}
       </div>
     </div>
